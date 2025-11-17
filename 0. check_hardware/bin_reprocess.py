d35f2552a0c1e27ab3b94ba0686bef5e8ef81c2c
Lee <jk06033@knu.ac.kr>
2025-11-17 22:22:29 +0900

    Add comments

--- 0. check_hardware/bin_reprocess.py
+++ 0. check_hardware/bin_reprocess.py
@@ -2,9 +2,12 @@ import cv2
 import os
 import glob
 
+
+# --- 설정 ---
+# 기존에 수집했던 버전 1의 원본 이미지 폴더
 origin_image_folder = r""
 
-
+# 새로 저장할 폴더
 reprocessed_folder = r""
 
 
@@ -16,15 +19,15 @@ image_paths = sorted(glob.glob(os.path.join(origin_image_folder, "*.png")))
 print(f"총 {len(image_paths)}개의 이미지를 재처리합니다...")
 
 for img_path in image_paths:
-    
+    # 원본 이미지 읽기 (그레이스케일로)
     gray_image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
     if gray_image is not None:
-        
+        # 색상 반전하여 이진화 적용        
         binarized = cv2.adaptiveThreshold(gray_image, 255, 
                                           cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                          cv2.THRESH_BINARY_INV,
-                                          51, -10) 
+                                          cv2.THRESH_BINARY_INV, # 색상 반전 옵션
+                                          51, -10) # 블록 크기와 상수 값 조정 가능
 
         filename = os.path.basename(img_path)
         save_path = os.path.join(reprocessed_folder, filename)
