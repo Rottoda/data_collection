3f90515f4f7ba8c78ba6124dc9c2cc63f7a7a8af
Junhui Lee <117970571+EdwardLee6677@users.noreply.github.com>
2025-12-06 12:20:52 +0000

    add code

--- data_collection_2points.py
+++ data_collection_2points.py
@@ -8,3 +8,29 @@ import cv2
 from datetime import datetime
 import nidaqmx
 from nidaqmx.constants import AcquisitionType, Edge
+
+
+CONFIG = {
+
+        # 고정 회전 각도
+        "radi" : -277.4,
+
+        # 카메라 인덱스
+        "camera_index": 0,
+
+        # 로봇 속도 (1~100)
+        "robot_speed": 50,
+
+        # 로봇의 안전높이 (눌림의 위치보다 얼마나 위로 이동할지, 단위: mm)
+        "safe_height_offset": 20.0,
+
+        # 로봇의 ip 주소
+        "robot_ip": "192.168.1.6",
+
+        # FT 센서 설정
+        "ft_samples": 100,
+        "ft_rate": 1000,
+        "ft_time": 1.0
+}
+
+# =====================================================================
